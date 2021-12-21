37b1ddb26e43d1807e3cdc458f497e21b20d8605
Paweł <pawel.wasowski@angrynerds.co>
2021-12-21 18:00:24 +0100

    Git ignore added

--- UsingCheck/Program.cs
+++ UsingCheck/Program.cs
@@ -1,22 +1,32 @@
-﻿TraverseTree("C:\\Dev\\VapianoPortal\\src");
+﻿global using System.Text.Json;
+
+TraverseTree("C:\\Dev\\VapianoPortal\\src");
 
 Console.WriteLine("Press any key");
 Console.ReadKey();
 
 static void TraverseTree(string root)
 {
+    
     // Data structure to hold names of subfolders to be
     // examined for files.
     Stack<string> dirs = new Stack<string>();
 
+    var usingsStatistic = new List<UsingCount>();
+
     if (!Directory.Exists(root))
     {
-        throw new ArgumentException();
+        throw new ArgumentException("Folder does not exists.");
     }
+
     dirs.Push(root);
 
     while (dirs.Count > 0)
     {
+        if (dirs.Contains("frontend"))
+        {
+            continue;
+        }
         string currentDir = dirs.Pop();
         string[] subDirs;
         try
@@ -60,13 +70,41 @@ static void TraverseTree(string root)
         }
         // Perform the required action on each file here.
         // Modify this block to perform your required task.
+
+        var progressIndicator = 0;
         foreach (string file in files)
         {
+            if (file.Contains("Migrations") || file.Contains("bin") || file.Contains("obj"))
+            {
+                continue;
+            }
+            progressIndicator++;
+            Console.WriteLine($"Progress: { progressIndicator } / {files.Count()}");
+            var usingsInFile = new List<string>();
+
             try
             {
-                // Perform whatever action is required in your scenario.
-                FileInfo fi = new FileInfo(file);
-                Console.WriteLine("{0}: {1}, {2}", fi.Name, fi.Length, fi.CreationTime);
+                var lines = File.ReadAllLines(file);
+                foreach (var line in lines)
+                {
+                    if (line.StartsWith("using"))
+                    {
+                        usingsInFile.Add(line);
+                    }
+                }
+
+                foreach (var singleUsing in usingsInFile)
+                {
+                    var existingUsing = usingsStatistic.FirstOrDefault(x => x.UsingName.Equals(singleUsing));
+                    if (existingUsing is not null)
+                    {
+                        existingUsing.Count++;
+                    }
+                    else
+                    {
+                        usingsStatistic.Add(new UsingCount(singleUsing));
+                    }
+                }
             }
             catch (FileNotFoundException e)
             {
@@ -78,9 +116,28 @@ static void TraverseTree(string root)
             }
         }
 
+        usingsStatistic = usingsStatistic.OrderByDescending(x => x.Count).ToList();
+
         // Push the subdirectories onto the stack for traversal.
         // This could also be done before handing the files.
         foreach (string str in subDirs)
             dirs.Push(str);
     }
+
+    var json = JsonSerializer.Serialize(usingsStatistic);
+    File.WriteAllText("Usings.json", json);
 }
+
+class UsingCount
+{
+    public UsingCount(string usingName)
+    {
+        UsingName = usingName;
+        Count = 1;
+    }
+
+    public string UsingName { get; set; }
+    public int Count { get; set; }
+}
+
+//Delete empty regions regex: #region Usings\n\n#endregion\n\n
